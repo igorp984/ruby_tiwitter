80157913a13d9579ec63888854d5d79fe3665e38
Igor Paim <igorp984@gmail.com>
2019-02-12 07:20:59 -0300

    tentando schedule sidekiq

---
+++ rails_app/config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+require 'sidekiq'
+
+Sidekiq.configure_server do |config|
+    config.redis = { url: 'redis://localhost:6380/0' }
+end
+   
+Sidekiq.configure_client do |config|
+    config.redis = { url: 'redis://localhost:6380/0' }
+end
